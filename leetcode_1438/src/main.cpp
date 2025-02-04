db0440a89f9eb8793ccf2b42bd7979a88bdf6fd0
Abubakir Koshek <koshek.a@phystech.edu>
2025-02-04 22:47:03 +0500

    solved 1438

--- leetcode_1438/src/main.cpp
+++ leetcode_1438/src/main.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    vector<int> nums = {8, 2, 4, 7};
-    int limit = 4;
-    cout << Solution().longestSubarray(nums, limit) << '\n'; // Expected: 2
+    vector<int> nums = {4,2,2,2,4,4,2,2};
+    int limit = 0;
+    cout << Solution().longestSubarray(nums, limit) << '\n'; // Expected: 4
     return 0;
 }
--- leetcode_1438/src/solution.cpp
+++ leetcode_1438/src/solution.cpp
@@ -1,8 +1,28 @@
 #include "../include/solution.h"
 #include <vector>
+#include <deque>
 using namespace std;
 
 
 int Solution::longestSubarray(vector<int>& nums, int limit) {
-    return 0;
+    int longestSubarrayLength = 1;
+    deque<int> max_deque = {0}, min_deque = {0};
+    int windowStartIndex = 0;
+    int current_index = 1;
+    while(current_index < nums.size()) {
+        while (!max_deque.empty() && (nums[current_index] < nums[max_deque.front()] - limit || 
+                                   nums[current_index] > nums[min_deque.front()] + limit)) {
+            ++windowStartIndex;
+            if (max_deque.front() < windowStartIndex) max_deque.pop_front();
+            if (min_deque.front() < windowStartIndex) min_deque.pop_front();
+        }
+        // updating max_deque and min_deque with current_index
+        while (!max_deque.empty() && nums[current_index] >= nums[max_deque.back()]) max_deque.pop_back();
+        while (!min_deque.empty() && nums[current_index] <= nums[min_deque.back()]) min_deque.pop_back();
+        max_deque.push_back(current_index);
+        min_deque.push_back(current_index);
+        ++current_index;
+        longestSubarrayLength = max(current_index - windowStartIndex, longestSubarrayLength);
+    }
+    return longestSubarrayLength;
 }
