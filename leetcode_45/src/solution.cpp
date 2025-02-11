6d524e1d7f0f8610e0109c14fde3e9b464b4ceda
Abubakir Koshek <koshek.a@phystech.edu>
2025-02-11 22:06:41 +0500

    solved leetcode 45

--- leetcode_45/src/solution.cpp
+++ leetcode_45/src/solution.cpp
@@ -1,7 +1,26 @@
 #include "../include/solution.h"
 #include <vector>
+#include <deque>
+#include <cassert>
 using namespace std;
 
 int Solution::jump(vector<int>& nums) {
-    return 0;
+    assert(nums.size() > 0);
+    int current_index = 0;
+    int steps = 0;
+    int max_index_for_current_steps = 0;
+    int max_index_for_one_more_steps = -1;
+    int index_to_reach = (int) nums.size() - 1;
+    while(current_index != index_to_reach) {
+        int max_reach_index = current_index + nums[current_index];
+        if (current_index > max_index_for_current_steps) {
+            ++steps;
+            max_index_for_current_steps = max_index_for_one_more_steps;
+            max_index_for_one_more_steps = max_reach_index;
+        }
+        if (max_reach_index > max_index_for_one_more_steps) max_index_for_one_more_steps = max_reach_index;
+        if (max_reach_index >= index_to_reach) return steps + 1;
+        ++current_index;
+    }
+    return steps;
 }
