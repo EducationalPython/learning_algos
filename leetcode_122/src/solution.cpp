386f95d3abbb71280276af63aea4a4df82c5c766
Abubakir Koshek <koshek.a@phystech.edu>
2025-02-11 14:34:16 +0500

    solved leetcode 122

--- leetcode_122/src/solution.cpp
+++ leetcode_122/src/solution.cpp
@@ -3,5 +3,13 @@
 using namespace std;
 
 int Solution::maxProfit(vector<int>& prices) {
-    return 0;
+    int max_profit = 0;
+    int buy_index = 0;
+    while(buy_index < prices.size()) {
+        int sell_index = buy_index;
+        while(sell_index + 1 < prices.size() && prices[sell_index + 1] >= prices[sell_index]) ++sell_index;
+        max_profit += prices[sell_index] - prices[buy_index];
+        buy_index = sell_index + 1;
+    }
+    return max_profit;
 }
