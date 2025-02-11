7d00ba64a5345e3f36abf8045a2eb1a0d846bf13
Abubakir Koshek <koshek.a@phystech.edu>
2025-02-11 23:00:29 +0500

    solved leetcode 274

--- leetcode_274/src/solution.cpp
+++ leetcode_274/src/solution.cpp
@@ -1,8 +1,16 @@
 #include "../include/solution.h"
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 
 int Solution::hIndex(vector<int>& citations) {
-    return 0;
+    sort(citations.begin(), citations.end(), std::greater<int>());
+    int h_index = 0;
+    while(h_index < citations.size()) {
+        if (citations[h_index] >= h_index + 1) {
+            ++h_index;
+        } else return h_index;
+    }
+    return h_index;
 }
