25525cfd707a9fc96990535ecb4b499ce504706d
Abubakir Koshek <koshek.a@phystech.edu>
2025-02-11 15:06:47 +0500

    solved leetcode 55

--- leetcode_55/src/solution.cpp
+++ leetcode_55/src/solution.cpp
@@ -1,7 +1,16 @@
 #include "../include/solution.h"
 #include <vector>
+#include <cassert>
 using namespace std;
 
 bool Solution::canJump(vector<int>& nums) {
-    return false;
+    assert(nums.size() > 0);
+    int index_to_reach = (int) nums.size() - 1;
+    while (index_to_reach) {
+        int previous_index = index_to_reach - 1;
+        while (previous_index >= 0 && previous_index + nums[previous_index] < index_to_reach) --previous_index;
+        if (previous_index == -1) return false;
+        index_to_reach = previous_index;
+    }
+    return true;
 }
